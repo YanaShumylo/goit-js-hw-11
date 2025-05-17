1a403fe7635d2deef1ca183a1d855cb98e7fb839
ShumyloYana <zolotushko1@gmail.com>
2025-05-18 01:11:06 +0300

    html new

--- src/js/pixabay-api.js
+++ src/js/pixabay-api.js
@@ -26,48 +26,4 @@ export async function getImagesByQuery(query) {
   }
 }
 
-// const form = document.querySelector('.js-form');
 
-// query.formElement.addEventListener('submit', (e) => {
-//     e.preventDefault();
-//     const userValue = e.target.elements.search-text.value;
-
-//     getImagesByQuery(userValue).then(data => {
-//         const markup = imageTemplate(data);
-//         form.insertAdjacentHTML('beforebegin', markup);
-//         e.target.reset();
-//     })
-// })
-
-// // ?-------------------------------------------------------------------------
-
-// function imageTemplate(image) {
-//   return `<li class="gallery-item">
-//      <a class="gallery-link" href="${image.original}">
-//       <img
-//         class="gallery-image"
-//         src="${image.preview}"
-//         alt="${image.description}"
-//       />
-//     </a>
-//   </li>`;
-// }
-
-// function imagesTemplate(images) {
-//   return images.map(imageTemplate).join('');
-// }
-
-// function renderImages() {
-//   const markup = imagesTemplate(images);
-//   container.innerHTML = markup;
-//   }
-// renderImages();
-
-// import SimpleLightbox from "simplelightbox";
-// import "simplelightbox/dist/simple-lightbox.min.css";
-
-// const lightbox = new SimpleLightbox('.gallery a', {
-//   captionsData: 'alt',      // текст підпису береться з alt
-//     captionDelay: 250,        // затримка перед появою підпису
-// });
-// user_id:11450328
